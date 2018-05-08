1094f0c3aeb91d3950386739f5b5378b529667c5
GabrielMint <augusto_costa96@hotmail.com>
2018-05-07 21:01:30 -0300

    Adicionado repository e service para a categoria

---
+++ src/main/java/com/gabrielmint/cursomc/service/CategoriaService.java
@@ -0,0 +1,24 @@
+package com.gabrielmint.cursomc.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gabrielmint.cursomc.domain.Categoria;
+import com.gabrielmint.cursomc.repository.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+	
+	
+	
+}
