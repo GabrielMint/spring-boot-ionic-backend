9e0490873ed67f4bb8203b4191c44587d2519d62
GabrielMint <augusto_costa96@hotmail.com>
2018-05-09 04:07:52 -0300

    Endpoint Cliente Disponivel

---
+++ src/main/java/com/gabrielmint/cursomc/service/ClienteService.java
@@ -0,0 +1,26 @@
+package com.gabrielmint.cursomc.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gabrielmint.cursomc.domain.Cliente;
+import com.gabrielmint.cursomc.repository.ClienteRepository;
+import com.gabrielmint.cursomc.service.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto Não Encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+	
+	
+	
+}
