a97b8bed5b64537c774ae6c0fcbefaf2fc1118ae
GabrielMint <augusto_costa96@hotmail.com>
2018-05-15 02:21:34 -0300

    validação customizada email nao repetido na atualização de cliente

--- src/main/java/com/gabrielmint/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/gabrielmint/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.gabrielmint.cursomc.domain.Cliente;
+import com.gabrielmint.cursomc.service.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
