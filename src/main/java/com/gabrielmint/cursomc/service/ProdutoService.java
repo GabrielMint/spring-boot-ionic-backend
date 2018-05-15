00406f65c365052e11bad64903f48be925456a77
GabrielMint <augusto_costa96@hotmail.com>
2018-05-15 19:40:38 -0300

    Busca de produtos por nome e categoria

--- src/main/java/com/gabrielmint/cursomc/service/ProdutoService.java
+++ src/main/java/com/gabrielmint/cursomc/service/ProdutoService.java
@@ -1,12 +1,17 @@
 package com.gabrielmint.cursomc.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
-
+import com.gabrielmint.cursomc.domain.Categoria;
 import com.gabrielmint.cursomc.domain.Produto;
+import com.gabrielmint.cursomc.repository.CategoriaRepository;
 import com.gabrielmint.cursomc.repository.ProdutoRepository;
 
 @Service
@@ -14,12 +19,18 @@ public class ProdutoService {
 	
 	@Autowired
 	private ProdutoRepository repo;
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 	
 	public Produto find(Integer id) {
 		Optional<Produto> obj = repo.findById(id);
 		return obj.orElse(null);
 	}
 	
-	
+	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linerPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linerPerPage, Direction.valueOf(direction), orderBy);
+		List<Categoria> categorias = categoriaRepository.findAllById(ids);
+		return repo.findDistinctByNomeContainingAndCategoriasIn(nome , categorias, pageRequest);
+	}
 	
 }
