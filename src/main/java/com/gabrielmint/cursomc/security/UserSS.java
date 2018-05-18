5f9101467eb7b3ebf49ce79c8974f2508cffe2eb
GabrielMint <augusto_costa96@hotmail.com>
2018-05-18 13:15:14 -0300

    Correção: Autenticação e Geração de Token

--- src/main/java/com/gabrielmint/cursomc/security/UserSS.java
+++ src/main/java/com/gabrielmint/cursomc/security/UserSS.java
@@ -56,17 +56,17 @@ public class UserSS implements UserDetails{
 
 	@Override
 	public boolean isAccountNonLocked() {
-		return false;
+		return true;
 	}
 
 	@Override
 	public boolean isCredentialsNonExpired() {
-		return false;
+		return true;
 	}
 
 	@Override
 	public boolean isEnabled() {
-		return false;
+		return true;
 	}
 	
 }
